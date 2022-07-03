e7b575ac58bc4ddce2ab8dbe69fe67a130e9d12c
malbannai <moudhi.albannai@gmail.com>
2022-07-03 19:55:17 +0300

    Done from first python task

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,12 @@
 def main():
-	# write your code here
+	print("Mad libs where libs get mad. \nStart below:\n")
+	num = input("Enter a number from 1 to 12: ")
+	noun = input("Enter a noun (plural): ")
+	name = input("Enter a name: ")
+	sent = input("Enter any sentence: ")
+	varb = input("Enter a verb: ")
+	print(f"\nThe story goes...\nIt was {num} o'clock when I heard a knock at the door. I opened the door and there was a box full of {noun} with a note saying \"From Mr. {name}.\" Just as I closed the door I heard a scream \"{sent.upper()}.\" I froze in place and all I could do was {varb}.")
+
 
 
 
