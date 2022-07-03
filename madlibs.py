e5a8280913230dc559a120916405e1286764a6a3
malbannai <moudhi.albannai@gmail.com>
2022-07-03 20:02:24 +0300

    Done from first python task

--- madlibs.py
+++ madlibs.py
@@ -1,11 +1,11 @@
 def main():
 	print("Mad libs where libs get mad. \nStart below:\n")
-	number = input("Enter a number from 1 to 12: ")
-	noun = input("Enter a noun (plural): ")
+	time = input("Enter a time from 1 to 12: ")
+	items = input("Enter a items (plural): ")
 	name = input("Enter a name: ")
-	sent = input("Enter any sentence: ")
-	varb = input("Enter a verb: ")
-	print(f"\nThe story goes...\nIt was {number} o'clock when I heard a knock at the door. I opened the door and there was a box full of {noun} with a note saying \"From Mr. {name}.\" Just as I closed the door I heard a scream \"{sent.upper()}.\" I froze in place and all I could do was {varb}.")
+	scream = input("Enter any sentance: ")
+	action = input("Enter a verb: ")
+	print(f"\nThe story goes...\nIt was {time} o'clock when I heard a knock at the door. I opened the door and there was a box full of {items} with a note saying \"From Mr. {name}.\" Just as I closed the door I heard a scream \"{scream.upper()}.\" I froze in place and all I could do was {action}.")
 
 
 
